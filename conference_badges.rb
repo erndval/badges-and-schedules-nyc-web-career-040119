ce9597e1fe3f8e2dbb1ff7c39a11e9e2898b6223
Ernie Val <erndval@gmail.com>
2019-03-16 01:28:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,6 @@ end
   def batch_badge_creator(list_of_names)
     list_of_badges = []
     list_of_names.each do |speaker|
-      "Hello, my name is #{speaker}" << list_of_badges
+      list_of_badges.push("Hello, my name is #{speaker}.")
     end
 end
